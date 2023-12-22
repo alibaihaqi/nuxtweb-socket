ffe87da4ef97bf9924866b413aba8770cff8b3dd
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-22 23:35:58 +0700

    feat: add home button component

---
+++ components/Home/HomeButton.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+defineProps<{
+  title: string
+  btnType?: string
+}>()
+</script>
+
+<template>
+  <button
+    :class="`
+      py-2 rounded font-bold
+      ${ btnType ? `btn-${btnType}` : 'btn-primary' }
+    `"
+  >
+    {{ title }}
+  </button>
+</template>
+
+<style scoped>
+.btn-primary {
+  @apply bg-blue-500 text-white;
+}
+
+.btn-secondary {
+  @apply bg-white text-gray-600 border border-gray-600;
+}
+</style>
