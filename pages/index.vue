b0406fddc1a6fed512a0e8b2d14a019cfce8854a
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-22 23:36:07 +0700

    feat: init pages

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="min-h-screen flex justify-center items-center">
+    <div :class="`
+        flex flex-col justify-around items-center w-3/4 h-96
+        max-w-md border border-black rounded-md shadow-sm px-8
+      `"
+    >
+      <h1 class="text-3xl font-bold text-blue-500">Nuxtweb Socket</h1>
+
+      <section class="flex flex-col gap-4 w-full">
+        <HomeButton
+          title="Join a Meeting"
+          btnType="primary"
+        />
+
+        <HomeButton
+          title="Host a Meeting"
+          btnType="secondary"
+        />
+      </section>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import HomeButton from '@/components/Home/HomeButton.vue'
+</script>
+
---
+++ pages/room/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="min-h-screen flex justify-center items-center">
+    <h1>Hello Room Index</h1>
+  </div>
+</template>
