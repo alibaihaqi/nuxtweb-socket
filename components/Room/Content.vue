e4ab0d8c37e998db8f9306e40d9b98ee82166ce5
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-12-27 05:29:19 +0700

    feat: init room content and footer components

---
+++ components/Room/Content.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-1 bg-gray-900 text-white">
+    CONTENT
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ components/Room/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-gray-500 p-2">
+    FOOTER
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
