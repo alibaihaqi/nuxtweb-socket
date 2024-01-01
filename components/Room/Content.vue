f746d20cea0f6584cea0cd4ce2f0b6a8a4445ba5
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-02 00:53:47 +0700

    feat: update room content component

--- components/Room/Content.vue
+++ components/Room/Content.vue
@@ -1,9 +1,35 @@
 <template>
-  <div class="flex flex-1 bg-gray-900 text-white">
-    CONTENT
+  <div class="flex flex-1 bg-gray-800 text-white">
+    <section class="flex flex-col flex-1">
+      <div class="self-center py-1 px-8 mt-1 bg-gray-700 rounded-md">
+        <p class="text-sm">Room ID: <span class="hover:text-blue-400 hover:cursor-pointer" @click="onClickCopyRoomId">{{ roomStore.roomId }}</span></p>
+      </div>
+
+      <section id="videos_container" class="flex-1" />
+    </section>
+
+    <Participants v-if="roomStore.isShowParticipants" />
   </div>
 </template>
 
 <script lang="ts" setup>
+import Participants from '@/components/Room/Participants.vue'
+import { useRoomStore } from '@/stores/room';
 
+const roomStore = useRoomStore()
+
+const onClickCopyRoomId = async () => {
+  await navigator.clipboard.writeText(roomStore.roomId)
+}
 </script>
+
+<style scoped>
+.videos_container_styles {
+  @apply flex flex-wrap w-[95%] mx-auto py-2 gap-2 justify-center;
+}
+
+.video_container_style {
+  @apply flex-1;
+}
+
+</style>
