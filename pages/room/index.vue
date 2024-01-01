f50094655de54ebb6561054b4f7259d0d3cb1202
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-02 00:52:59 +0700

    feat: update room page

--- pages/room/index.vue
+++ pages/room/index.vue
@@ -8,4 +8,24 @@
 <script lang="ts" setup>
 import RoomContent from '@/components/Room/Content.vue'
 import RoomFooter from '@/components/Room/Footer.vue'
+import { useRoomStore } from '@/stores/room';
+
+const meetingStore = useRoomStore()
+
+onMounted(() => {
+  initiateRoom()
+})
+
+const initiateRoom = async () => {
+  meetingStore.setIsInitiateRoom(true)
+
+  await getLocalPreviewAndRoomConnection({
+    audioToConnect: meetingStore.isMicrophoneActive,
+    meetingId: meetingStore.roomId,
+    meetingName: meetingStore.meetingName,
+    isHostMeeting: meetingStore.isHostMeeting,
+  })
+
+  meetingStore.setIsInitiateRoom(false)
+}
 </script>
