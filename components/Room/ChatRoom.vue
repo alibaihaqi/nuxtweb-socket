20f6f7ed4c98c980fd20c0c6924af24dbe8b9503
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-05 15:50:53 +0700

    feat: init chat room component

---
+++ components/Room/ChatRoom.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="hidden sm:inline-flex sm:w-52 md:w-80 flex flex-col flex-1 bg-gray-700 border-t border-white">
+    CHAT ROOM
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
