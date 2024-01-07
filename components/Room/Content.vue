04f22c3ad8e3f38ad112e07679585118f9471f76
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-07 22:30:28 +0700

    feat: update styling content room

--- components/Room/Content.vue
+++ components/Room/Content.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="flex flex-1 bg-gray-800 text-white">
     <section class="flex flex-col flex-1">
-      <div class="self-center py-1 px-8 mt-1 bg-gray-700 rounded-md">
-        <p class="text-sm">Room ID: <span class="hover:text-blue-400 hover:cursor-pointer" @click="onClickCopyRoomId">{{ roomStore.roomId }}</span></p>
+      <div class="self-center py-1 px-4 sm:px-8 mt-2 bg-gray-700 rounded-md">
+        <p class="text-sm">
+          Room ID: <span class="hover:text-blue-400 hover:cursor-pointer" @click="onClickCopyRoomId">{{ roomStore.roomId }}</span>
+        </p>
       </div>
 
-      <section id="videos_container" class="flex-1" />
+      <section id="videos_container" />
     </section>
 
     <section class="flex flex-col">
@@ -28,13 +30,13 @@ const onClickCopyRoomId = async () => {
 }
 </script>
 
-<style scoped>
+<style>
 .videos_container_styles {
-  @apply flex flex-wrap w-[95%] mx-auto py-2 gap-2 justify-center;
+  @apply flex flex-col sm:flex-row flex-wrap flex-1 w-[95%] mx-auto py-2 gap-2 justify-center;
 }
 
 .video_container_style {
-  @apply flex-1;
+  @apply flex flex-1;
 }
 
 </style>
