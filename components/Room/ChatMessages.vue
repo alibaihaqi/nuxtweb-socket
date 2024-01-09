bb3871dd9c42335dc99d7a419db5d6e90e013f17
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-09 16:18:10 +0700

    feat: add chat messages component

---
+++ components/Room/ChatMessages.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="flex flex-col flex-1 px-2 pb-2 gap-2 overflow-hidden overflow-y-auto">
+    <div
+      v-for="(message, idx) in messages"
+      :key="idx"
+      class="flex flex-col gap-1"
+      :class="isAuthorSocketId(message) ? 'items-end text-right' : 'items-start text-left'"
+    >
+      <p
+        v-if="idx === 0 || idx > 0 && !(message.socketId === messages[idx - 1].socketId)"
+        class="text-xs"
+      >
+        {{ getAuthorMessage(message) }}
+      </p>
+      <div
+        class="px-2 py-1 rounded-md max-w-[60%]"
+        :class="isAuthorSocketId(message) ? 'bg-blue-500' : 'bg-blue-400'"
+      >
+        <p class="text-sm text-gray-100">
+          {{ message.content }}
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+import type { IChatMessage } from '~/interfaces/room/room';
+
+const props = defineProps<{
+  messages: IChatMessage[]
+  authorSocketId: string
+}>()
+
+const isAuthorSocketId = (message: any): boolean => {
+  return message.socketId === props.authorSocketId
+}
+
+const getAuthorMessage = (message: any) => {
+  return isAuthorSocketId(message) ? 'You' : message.name
+}
+</script>
