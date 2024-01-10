a4b72406e31f7290f40cc970f3080d3214377a89
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2024-01-11 00:01:56 +0700

    feat: update head title nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'WebRTC alibaihaqi',
+    },
+  },
+
   devtools: {
     enabled: true,
   },
